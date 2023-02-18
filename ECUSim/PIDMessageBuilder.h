33060bfebe7063b0a6709cc9ba527c1b56ebdc65
sz2 <sugiura.k.nigo@gmail.com>
2023-02-18 17:26:23 +0900

    Change function export.

--- ECUSim/PIDMessageBuilder.h
+++ ECUSim/PIDMessageBuilder.h
@@ -1,6 +1,7 @@
 #pragma once
 
+constexpr int NOERROR = 0;
+constexpr int PID_NOT_AVAILABLE = -1;
+
 int  buildPIDValueMessage(byte* const returnBuf, uint8_t& returnByteCount, const uint8_t* requestedPIDList, const uint8_t requestedPIDCount, const uint8_t returnServiceMode);
-void fillValueBytes(byte* const returnBuf, const uint8_t requestedPID, const uint8_t byteOffsetToFill, uint8_t& byteOffsetAfterFill);
-void fillAvailablePIDBytes(byte* const returnBuf, const uint8_t requestedPID, const uint8_t byteOffsetToFill, uint8_t& byteOffsetAfterFill);
 
--- ECUSim/PIDMessageBuilder.ino
+++ ECUSim/PIDMessageBuilder.ino
@@ -1,3 +1,8 @@
+#include "PIDMessageBuilder.h"
+
+void fillValueBytes(byte* const returnBuf, const uint8_t requestedPID, const uint8_t byteOffsetToFill, uint8_t& byteOffsetAfterFill);
+void fillAvailablePIDBytes(byte* const returnBuf, const uint8_t requestedPID, const uint8_t byteOffsetToFill, uint8_t& byteOffsetAfterFill);
+
 int  buildPIDValueMessage(byte* const returnBuf, uint8_t& returnByteCount, const uint8_t* requestedPIDList, const uint8_t requestedPIDCount, const uint8_t returnServiceMode)
 {
   returnBuf[1] = returnServiceMode;
